7a7a26bfb6c5b37ea2ad057e9317a29328d8067e
kevin <ksabbe@ext-wedigitalgarden.com>
2017-03-21 00:56:12 +0100

    update

---
+++ EasySwift/EasySwift/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EasySwift
+//
+//  Created by Sabbe on 21/03/17.
+//  Copyright © 2017 sabbe.kev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
