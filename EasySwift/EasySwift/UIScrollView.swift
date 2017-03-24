e3502d645b47d3ffefcb9e15d4ee2e6f726d678d
kevin <ksabbe@ext-wedigitalgarden.com>
2017-03-24 16:25:35 +0100

    add scrollView methods

--- EasySwift/EasySwift/UIScrollView.swift
+++ EasySwift/EasySwift/UIScrollView.swift
@@ -32,5 +32,25 @@ import UIKit
 
 extension UIScrollView
 {
+    func scrollToView(view: UIView, animated: Bool)
+    {
+        if let origin = view.superview
+        {
+            let childStartPoint = origin.convert(view.frame.origin, to: self)
+            self.scrollRectToVisible(CGRect(x: 0, y: childStartPoint.y, w: 1, h: self.frame.height), animated: animated)
+        }
+    }
     
+    func scrollToTop(animated: Bool)
+    {
+        let topOffset = CGPoint(x: 0, y: -contentInset.top)
+        setContentOffset(topOffset, animated: animated)
+    }
+    
+    func scrollToBottom() {
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
+        if(bottomOffset.y > 0) {
+            setContentOffset(bottomOffset, animated: true)
+        }
+    }
 }
